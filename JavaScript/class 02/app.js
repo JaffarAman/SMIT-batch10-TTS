19b608c9c8d87c22d6e44da115d82dad7e721f33
Jaffar <amanjaffar50@gmail.com>
2023-09-05 23:04:37 +0500

    JAVASCTIPT START

---
+++ JavaScript/class 02/app.js
@@ -0,0 +1,65 @@
+// alert("HELLO 2")
+// alert("HELLO 3")
+// alert("HELLO 1")
+
+
+// translate programs written in high-level languages into machine code that a computer understands
+// compiler => read all code then generate output
+// interpreter => line by line
+// assembler =>  assembler lang
+
+
+// JS use  interpreter translator 
+
+
+
+
+// Variables in JS
+
+
+
+// // =  assigning opt
+// var username = "JAFFAR AMAN"; 
+
+// // statement terminator ;
+
+
+// console.log(username)
+
+
+
+// var num1 = 500
+// num1 = 3000
+
+
+// console.log(num1)
+
+
+
+// var num1 = 500
+// console.log("1st", num1)
+// num1 = 3000
+// console.log("2nd", num1)
+
+var num1 = 100;
+var num2 = 200;
+// var result = num1 + num2
+var result = 100 + 200
+//100 + 200
+
+
+console.log(result)
+
+
+
+
+
+
+
+
+
+// a = 500
+// b = 100
+
+// console.log(a)
+// console.log(b)
