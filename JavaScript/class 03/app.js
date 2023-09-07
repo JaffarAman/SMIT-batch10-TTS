4768debc00348962edf8a1657ebd701f49e9b44c
Jaffar <amanjaffar50@gmail.com>
2023-09-07 22:27:13 +0500

    data types in js

---
+++ JavaScript/class 03/app.js
@@ -0,0 +1,138 @@
+// alert("HELLO WORLD")
+
+// 100 200
+
+// var num1 = 100
+// var num2 = 200
+
+// console.log(num1)
+// console.log(num2)
+
+
+// // PROGRAM: add two numbers
+// var num1 = 450;
+// var num2 = 650;
+
+// var add = num1 + num2;
+// //450 + //650
+
+// console.log("addition", add)
+
+
+
+// var=> keyword
+// firstname => variable name
+// =   => assiging Option
+// "Jaffar" => variable value
+// var firstname = "Jaffar";
+// var lastname = "Aman";
+
+
+
+// var 1num = 500 =>error
+// var 500 = 500 =>error
+
+// var $num = 500 => done
+// var _num1 = 1000=>done
+
+// var first_name = "Jaffar"
+
+
+// var num = 1000;
+
+
+// firstname
+
+// JAFFAR
+
+// JAFFAR
+// Jaffar
+// jaffar
+
+// FirstName
+// first_name
+
+
+// firstname
+// firstName
+
+
+
+
+
+
+
+
+// var firstName = "Jaffar";
+// console.log(a)
+
+
+// var firstName = "Sufiyan";
+// var num1 = 200;
+
+
+// var add = firstName + num1;
+// console.log(add)
+
+
+
+// // concatenation
+// var num1 = "200"
+// var num2 = "500"
+
+// var add = "500" + "200";
+// var add = 500 + 200;
+
+
+// console.log("add", add)
+
+
+// DATA TYPES
+// 1. string  =>  " aksjdklasjdl asdk "
+
+
+// 2. number =>   54  1000  101  202 303
+// 3. boolean
+// 4. null
+// 5. undefined
+
+
+// var fullName = "Jaffar";    //string
+// var num10 = "1000"  //Number Data types
+
+
+
+// // concatenation  => concat
+// var result = "500" + "1000"
+// var result = 500 + "1000"
+// console.log(result)
+
+
+
+
+// var result = 1000 + 2000
+// var result = "1000" + 2000
+// var result = "1000" + "2000"
+// var result = 1000 + "2000"
+// console.log(result)
+
+
+
+var num1 = "200";  //string
+var num2 = "50"; // string
+
+var add = num1 - num2
+var minus = num1 * num2
+console.log(add)
+
+
+
+
+
+
+
+
+
+
+
+
