6faef48df56fe9f27d4a83e4e0f2a36bdc70609c
Jaffar <amanjaffar50@gmail.com>
2023-09-12 22:31:16 +0500

    if statements

---
+++ JavaScript/class 04/app.js
@@ -0,0 +1,125 @@
+// alert("HELLO WORLD");
+
+
+// var num1 = 100
+// num1 = 200
+// console.log(num1)
+
+
+// var num1 = 10 + 20 * 2  //DMAS rule
+// var num1 = 2 * 5 - 10 + 20  //DMAS rule
+//            10   -  10  + 20
+//            10 - 30
+
+// var num1 = 5 + 10 - 2 * 10 + 10
+//  =  5 + 10 - 20 + 10
+
+// var num1 = 10 + 5 * (10 - 2) - 10 // BODMAS
+// 10 + 5 * (8) - 10
+// 10 +  40 - 10
+
+
+
+// var num1 = 10 - 10 - 5 * 10 + (10 + 20 * 3) - 10
+// //    10 - 10 - 5 * 10 + 70 - 10
+// //    - 50 + 70 -10
+// // 20 - 10
+// 10
+
+
+
+// var num1 = 10 + 10 + "10"
+// var num1 = "10" + 10 + 10
+// "1010" + 10
+// "101010"
+
+
+// var num1 = 10 + "10" + 10
+// var num1 = "10" + (10 + 10)
+// var num1 = "10" - 20 + 10 + ("10" - 10)
+// var num1 = - 20 + "10" + 10 + ("10" - 10)
+//  - 20 + "10" + 10 + 0
+//  - 20 + "1010" + 0
+//  - 20 + "10100"
+// "-2010100"
+
+// var num1 = 100 + "0"
+// console.log("result", num1)
+
+
+
+// num1 = num1 + 5
+// num1 += 5
+
+// ++
+// --
+
+// Incremenet++
+// decremenet--
+
+// POST  Incremenet => phely print phr plus =>
+// PRE Incremenet =>pehly add karo phr print karo
+// var num1 = 50   //51
+// // console.log(num1++)  //50
+// console.log(++num1)  //51
+// console.log(num1)
+
+
+
+// var num1 = 50 // 49
+// // console.log(--num1) //
+// console.log(num1--) //
+// console.log(--num1) //
+
+// var resutl = num1++ + num1++ + ++num1
+// //   10    +  11    +   13
+
+// var result = num1 + ++num1 + num1++ + num1 + num1++
+//  10  +   11   +   11  +  12   +  12
+
+// var result = num1 + ++num1 + ++num1 + num1++ + ++num1 + num1
+//    10  +   11   +   12  +   12    +  14    + 14
+
+// // var result = num1 - num1++ + --num1 + num1++ + num1--
+// // var result = num1 - num1++ + --num1 + num1++ + num1--
+// // var num1 = 10  //11  / 12  //11  /10  / 9
+// // var result = num1 + ++num1 + num1++ + --num1 - num1-- - --num1
+// //  10   +  11    +   11    +  11   - 11   - 9 
+
+// var num1 = 12  //13  //12 //13  //12  //11
+// var result = num1++ - --num1 + num1 + ++num1 + num1-- - --num1
+//            // 12    -   12    + 12  +   13   +   13  -  11 
+
+// console.log("result", result)
+
+
+
+
+
+// var result = num1-- + num1++ + num1++ - --num1 - --num1 - num1-- + num1
+// var result = ++num1 + ++num1 - --num1 - num1 + num1
+// var result = num1 + num1 + ++num1 + --num1 - --num1 + ++num1 + --num1-num1
+
+
+// 71
+// 73
+// 77
+// 76
+
+
+// fullname
+// fullname
+
+// JS
+// camel case
+// fullName
+
+
+var result = undefined;
+var result;
+
+
+
+
+result = "PASS"
+console.log(result)
---
+++ JavaScript/class 05/app.js
@@ -0,0 +1,151 @@
+// alert("HELLO WORLD")
+// console.log("HELLO WORLD")
+
+// var
+// let
+// const
+
+
+// data types
+// String  ==> ""
+// Number ==>  20  , 50
+// Boolean ==>  true / false
+// undefined ==>
+// null  ==>
+
+
+// name
+// age
+// result
+// emailAddress
+// status
+
+// var stdName = "faysal"; //string
+// var stdAge = 16; // number
+// var stdResult = undefined //undefined
+// var stdEmail = null
+// var stdStatus = true // boolean
+// var stdStatus = "true" // string
+
+
+// console.log(stdName)
+// console.log(stdAge)
+// console.log(stdResult)
+// console.log(stdEmail)
+// console.log(stdStatus)
+
+
+
+
+
+// var fullName = "Jaffar Aman";   //string
+// var gender = 'male';   //string
+// var age = 22  //number
+// var result = undefined; //undefined
+
+
+// var fullName = undefined;
+// var fullName;
+// console.log(fullName)
+
+
+// var num1 = 10
+// var result = 11 % 2
+// console.log(result)
+
+
+// var userName = "Jaffar Aman"
+
+
+// built-in method;
+
+// var userName = prompt("enter your name")
+
+
+// console.log("username", userName)
+
+
+// var userAge = prompt("Enter your age")
+// console.log("userAge", userAge)
+
+// var firstName = prompt("Enter your first name")
+// var lastName = prompt("Enter your last name")
+// //jaffaraman 
+// // "jaffar" + " " + "aman"  
+// var fullName = firstName + " " + lastName
+// console.log("fullName", fullName)
+
+
+// name
+// age
+// result
+// emailAddress
+// status
+
+// var stdName = prompt("Enter your full name", "John Doe")
+// var stdAge = prompt("Enter your age")
+// var stdResult = prompt("Enter your result")
+// var stdEmail = prompt("Enter your email")
+
+// console.log(stdName)
+// console.log(stdAge)
+// console.log(stdEmail)
+// console.log(stdResult)
+
+
+
+
+
+
+// var num1 = +prompt("Enter num 1")
+// var num2 = +prompt("Enter num 2")
+
+// var add = num1 + num2
+// console.log("add", add)
+
+
+
+
+
+
+// var gender = "male"
+// if (gender = "male") {
+
+// } else {
+
+// }
+
+// var gender = "male"
+
+// if (gender == "male") {
+//     console.log("allow", gender)
+// } else {
+//     console.log("not allow")
+// }
+
+// var number1 = 201
+// if (number1 == 200) {
+
+//     console.log("Allow")
+
+// } else {
+//     console.log("not Allow")
+
+// }
+
+
+
+// if(condition){
+
+// }else{
+
+// }
+
+
+// var age = 50
+
+// if (age == 20) {
+//     console.log("ALLOW")
+// } else {
+//     console.log("NOT ALLOW")
+// }
