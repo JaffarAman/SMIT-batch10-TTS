562717fbd4cccfcfcb31267e03a31bdca5563a26
Jaffar <amanjaffar50@gmail.com>
2023-09-14 22:57:21 +0500

    if else condition in js

---
+++ JavaScript/class 06/app.js
@@ -0,0 +1,128 @@
+// alert("HELLO WORLD!")
+
+// var fullName = prompt("ENter your name")
+
+// console.log(fullName)
+
+// var num1 = +prompt("num1")
+// var num2 = +prompt("num2")
+
+// console.log(num1 + num2)
+
+
+
+
+// condition  if / else
+
+// allow age = 20
+
+// var stdAge = +prompt("Enter your age")
+
+// if (stdAge == 20) {
+//     console.log("Allow")
+// } else {
+//     console.log("not Allow");
+// }
+
+
+// var number = 100;
+
+// if (number === 100) {
+//     console.log("ALLOW")
+// } else {
+//     console.log("Not Allow")
+// }
+
+
+
+// 20
+// 25
+// 30
+// 35
+
+// if > true
+// else > false
+// else if => multiply condition
+
+// var stdAge = prompt("Enter your age")
+
+// if (stdAge == 20) {
+//     console.log("Allow")
+// } else if (stdAge == 25) {
+//     console.log("Allow")
+
+// } else if (stdAge == 30) {
+//     console.log("Allow")
+// } else if (stdAge == 35) {
+//     console.log("Allow")
+// } else {
+//     console.log("Not allow")
+// }
+
+
+
+// var gender = prompt("Enter your gender");
+
+
+// if (gender === "male") {
+//     console.log("Boys section")
+// } else if (gender === "female") {
+//     console.log("Women☕ section")
+// } else {
+//     console.log("not allow")
+// }
+
+// var num1 = +prompt("Enter num1")
+// var num2 = +prompt("Enter num2")
+// var opt = prompt("Enter opt", "+,-,*,/")
+
+
+// if (opt == "+") {
+//     console.log("Addition", num1 + num2)
+// } else if (opt == "-") {
+//     console.log("sub", num1 - num2)
+// } else if (opt == "/") {
+//     console.log("div", num1 / num2)
+// } else if (opt == "*") {
+//     console.log("mul", num1 * num2)
+// } else {
+//     console.log("wrong opt syntax error")
+// }
+
+// console.log(num1)
+// console.log(num2)
+// console.log(opt)
+
+
+
+
+var age = 20
+
+
+// >  ==> greater then
+// <  ==> less then
+// var stdAge = 15
+
+
+// >   ==> greater then
+// >=  ==  greater then or equal to
+// <   == less then
+// <=  ==  less then or equal to
+
+
+// if (stdAge >= 18) {
+//     console.log("ALLOW")
+// } else {
+//     console.log("not ALLOW")
+// }
+var stdAge = 18
+if (stdAge <= 18) {
+    console.log("techno kids")
+} else {
+    console.log("not allow")
+}
+
+
+
+
+
