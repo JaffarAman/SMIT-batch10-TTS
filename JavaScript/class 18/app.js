93d2298a462550e6618b96ccf6f8a427e59b2d01
mdabdullahbaloch <abdullah356abdulhameed@gmail.com>
2023-10-10 22:45:17 +0500

    chapter 30 complete

---
+++ JavaScript/class 18/app.js
@@ -0,0 +1,59 @@
+// var num1 = 200.50  //decimal
+// var num1 = 200 //integer
+// console.log(typeof (num1))
+
+// var num1 = 99.9
+// var round = Math.round(num1)
+// var num2 = 85.9
+// var floor = Math.floor(num2)
+// var num3 = 50
+// var ceil = Math.ceil(num3)
+
+
+// console.log("num1", num1)
+// console.log("round", round)
+// console.log("floor", floor)
+// console.log("ceil", ceil)
+
+// var randowNumber = Math.floor(Math.random() * 2 + 1)  // range  1 to 2
+// var randowNumber = Math.floor(Math.random() * 50 + 1) //range 1 to 50
+// var randowNumber = Math.floor(Math.random() * 6 + 1) //range 1 to 50
+
+
+// var randomNumber = Math.floor(Math.random() * 10 + 1)
+// var randomNumber = Math.ceil(Math.random() * 10)
+// console.log(randomNumber)
+
+
+// var num1 = +"100"
+// var num2 = "200"
+
+// var add = num1 + +num2
+// console.log("add", add)
+
+// console.log(typeof num1)
+// console.log(typeof num2)
+
+
+
+
+// convert String into number ==> Number()  / +
+// var num1 = Number("99.9")
+// console.log("num1 type", typeof num1)
+// console.log("num1 value", num1)
+
+
+// var num1 = parseInt("99.9")
+// console.log("num1 type", typeof num1)
+// console.log("num1 value", num1)
+
+
+// var num1 = "45"
+// console.log(Number(num1))
+
+// console.log(typeof Number(num1))
+
+
+
+
+
