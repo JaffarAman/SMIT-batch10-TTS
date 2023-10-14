282ace3e4b27d529d57f5e51ef3dd1e1de78956d
mdabdullahbaloch <abdullah356abdulhameed@gmail.com>
2023-10-14 22:51:27 +0500

    javascript function start

---
+++ JavaScript/class 19/exam.js
@@ -0,0 +1,16 @@
+/*
+DEADLINE: 12 PM
+1. Basic Quiz App with total score (60%)
+2. There should be a 30 seconds timer
+popup, you should show that timer to the user during the whole quiz.
+If the timer ends, it should end the
+quiz (20%)
+3. It should fetch the data from
+this API: https://opentdb.com/api.php?amount=10 
+and use this data for quiz (20%, BONUS)
+
+
+PASSING: 70%
+Allowed to copy: HTML, CSS & Data (Array of Objects)
+NOT ALLOWED to copy: Any JS logic
+*/
---
+++ JavaScript/class 20/app.js
@@ -0,0 +1,95 @@
+// console.log("HELLO WORLD")
+
+
+// var now = new Date().toDateString()
+// var now = new Date().toTimeString()
+// var now = new Date().toLocaleDateString()
+// var now = new Date().toLocaleTimeString()
+// var now = new Date()
+// console.log(now)
+// console.log(now.slice(0, 3))
+// console.log(now.slice(0 , 3))
+
+
+
+
+// var now = new Date()
+// var offerEndDate = new Date("oct 10 2023")
+// // console.log(now)
+// // console.log(offerEndDate)
+
+// var nowMS = now.getTime()
+// var offerDateMS = offerEndDate.getTime()
+// console.log(nowMS)
+// console.log(offerDateMS)
+// var diff = offerDateMS - nowMS
+// var remainingDays = Math.floor(diff / (1000 * 60 * 60 * 24))
+// console.log(remainingDays)
+// if (remainingDays > 0) {
+//     alert("OFFER VALID")
+// } else {
+//     alert("OFFER khatam gareebo")
+// }
+
+
+
+
+// var num1 = 100
+// var num2 = 200
+// var add1 = num1 + num2
+
+// console.log("add1", add1)
+
+
+// var num3 = 300
+// var num4 = 400
+// var add2 = num3 + num4
+// console.log("add2", add2)
+
+
+// console.log("HELLO WORLD")
+// console.log("HELLO WORLD")
+// console.log("HELLO WORLD")
+// console.log("HELLO WORLD")
+// console.log("HELLO WORLD")
+
+
+
+
+
+// parenthesis
+// function   function_name  ()   {
+
+// }
+
+
+
+// helloWorld
+
+// function helloWorld() {
+//     console.log("HELLO WORLD")
+// }
+
+// helloWorld()
+// helloWorld()
+// helloWorld()
+// helloWorld()
+// helloWorld()
+// helloWorld()
+// helloWorld()
+// helloWorld()
+// helloWorld()
+// helloWorld()
+
+// alert()
+
+
+
+function addition() {
+    console.log(100 + 200)
+
+}
+
+addition()
+addition()
+addition()
