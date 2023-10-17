276c231d115c395c69b831631d71bc8f799a2d0b
InnoSufiyan <innosufiyan@gmail.com>
2023-10-17 22:50:25 +0500

    functions in js

---
+++ JavaScript/class 21/app.js
@@ -0,0 +1,134 @@
+
+
+
+// function foo() {
+//     console.log("HELLO WORLD")
+//     alert("HELLO WORLD")
+
+// }
+
+// foo()
+// foo()
+// foo()
+
+
+
+
+// parameters
+// arguments
+
+// function add(parameters) {
+//     console.log(20 + 30)
+// }
+
+// add(argumenets)
+
+
+// function add(num1, num2) {
+//     console.log("num1", num1)
+//     console.log("num2", num2)
+// }
+
+// console.log(num1)
+// add(50, 80)
+
+// function add(num1, num2) {
+//     console.log(num1 + num2)
+// }
+
+
+// add(100, 200)
+// add(500, 200)
+
+
+
+// function foo(value1, value2) {
+//     console.log(value1)
+//     console.log(value2)
+// }
+
+
+// foo("Jaffar", "Aman", 22, "karachi") 
+
+
+
+
+// function userName(name, num1, num2, num3, num4) {
+//     console.log("name", name, num1, num2, num3, num4)
+// }
+
+
+// var namePrompt = prompt("Enter Your name", "Jaffar")
+// userName(namePrompt, 20, 50)
+
+
+
+
+// function foo(userName) {
+//     console.log(userName)
+//     var userName = "Jaffar"
+//     console.log(userName)
+// }
+
+// foo("Jaffar Aman")
+
+
+
+// function foo() {
+
+//     var num1 = 200
+//     console.log("hello 1", num1)
+//     return 500
+//     console.log("hhahahaahahaha")
+
+// }
+
+// var result = foo()
+
+// console.log(result)
+
+
+
+// function add(num1, num2) { //parameters
+//     return num1 + num2
+// }
+
+
+// var res1 = add(20, 30) //arguments
+// var res2 = add(80, 20) //arguments
+// var res3 = add(90, 20) //arguments
+
+
+
+
+// var userName = "jaffar"
+
+// function foo() {
+//     console.log(userName)
+// }
+
+// foo()
+// var hello;
+// var hello = "HELLO"
+
+
+
+var a = 10 ///global variables
+var b = 20
+
+
+
+
+function foo() {
+
+
+    var userName = "jaffar"  //local scope variable
+    console.log(userName)
+
+
+
+
+}
+
+foo()
+console.log(userName)
