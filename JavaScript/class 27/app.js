9880a7f4eb91c9a4fc47736e15e48aa24c88bb26
Jaffar Aman <amanjaffar50@gmail.com>
2023-11-04 22:39:55 +0500

    chapter 55 complete

---
+++ JavaScript/class 27/app.js
@@ -0,0 +1,107 @@
+// function foo() {
+//     var heading = document.getElementById("heading")
+//     // heading.className += " black"
+//     heading.classList.add("black", "addmulti")
+//     console.log("foo()", heading.className)
+// }
+
+
+// function removeBlackCLass() {
+//     var heading = document.getElementById("heading")
+//     // heading.className = "heading white"
+//     console.log(heading.classList)
+//     heading.classList.remove("black", "white")
+// }
+
+
+// switch statement
+
+
+// var age = 18    /// 18 // 25
+// // if (age === 18) {
+// //     console.log("ALLOW")
+// // } else {
+// //     console.log("NOT ALLOW")
+// // }
+// // case = > if
+// // default => else
+// switch (age) {
+//     case 30:
+//         console.log("ALLOW");
+//         break
+//     case 25:
+//         console.log("allow");
+//     default: console.log("NOT ALLOW");
+// }
+
+var dayOfWk = "Mon"
+// if (dayOfWk === "Sat" || dayOfWk === "Sun") {
+//     alert("Whoopee!");
+// }
+// else if (dayOfWk === "Fri") {
+//     alert("TGIF!");
+// }
+// else {
+//     alert("kam pr ja bhai ")
+// }
+
+// switch (dayOfWk) {
+//     case "Sat":
+//         alert("Weekend!");
+//         break
+//     case "Sun":
+//         alert("Funday!");
+//         break
+//     case "Fri":
+//         alert("Jumme ki rat!");
+//         break
+//     default:
+//         alert("Kam pr ja");
+
+// }
+
+
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// var i = 1
+// while (i <= 10) {
+//     console.log(i)
+
+
+
+//     i++
+// }
+
+
+// var i = 1
+// while (i <= 10) {
+
+//     // block statments
+//     console.log(i)
+
+
+
+
+//     i++
+
+// }
+
+
+
+var i = 1
+do {
+
+    console.log(i)
+
+
+    i++
+
+
+} while (false)
+
+
+
+
