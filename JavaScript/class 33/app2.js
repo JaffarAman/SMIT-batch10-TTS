c70e9de8d7cc72869d0f307fabcb102e6b0b1962
Jaffar Aman <amanjaffar50@gmail.com>
2023-11-18 21:30:00 +0500

    todo complete

---
+++ JavaScript/class 33/app2.js
@@ -0,0 +1,29 @@
+// console.log("HELLO WORLD 1")
+// console.log("HELLO WORLD 3")
+// console.log("HELLO WORLD 2")
+
+
+// setTimeout(  function(){}  , time   )
+// time => miliSecond
+
+
+// console.log("Hello World 1")
+
+// function foo() {
+//     console.log("Hello World 2")
+//     // console.log("HELLO WORLD AFTER 5 SEC")
+//     // data getC/omputedStyle
+//     //  maping date in UI
+// }
+
+// setTimeout(foo, 0)
+
+// console.log("Hello World 3")
+
+
+
+
+
+setInterval(function () {
+    console.log("HELLO WORLD")
+}, 2000);
