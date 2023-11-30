3e5b46e9490e54e20ee5499363147f1850792312
JaffarAman <amanjaffar50@gmail.com>
2023-11-30 22:22:57 +0500

    basic javascript complete

---
+++ JavaScript/class 37/app.js
@@ -0,0 +1,18 @@
+function aboutpage() {
+    console.log("aboutpage()")
+    // console.log(window.location.href)
+    // window.location.href = "./about.html"
+    // window.location.assign("./about.html")
+
+    // window.location.replace("./about.html")
+    // window.location.reload()
+    // window.location.host
+    // window.location.pathname
+
+
+}
+
+
+function openWindow() {
+    window.open("./about.html", "aboutpage", "width=450,height=450")
+}
