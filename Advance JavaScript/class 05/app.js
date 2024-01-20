b74ed4304218e74ec013622cb24bae7c9b79692e
Jaffar Aman <amanjaffar50@gmail.com>
2024-01-20 22:24:37 +0500

    advance js end

---
+++ Advance JavaScript/class 05/app.js
@@ -0,0 +1,61 @@
+const arr = [{ name: "Jaffar" }]
+
+console.log(arr, "app.js")
+
+
+const firstName = "Jaffar"
+const lastName = "Aman"
+
+// const userObj = {
+//     firstName :firstName,
+//     lastName:lastName,
+// }
+// const userObj = {
+//     firstName,
+//     lastName,
+// }
+// console.log("userObj", userObj)
+
+
+// export default arr
+// export {
+//     firstName,
+//     lastName
+// }
+
+// const numberArr = [1, 1010, 4, 5, 8, 4]
+// numberArr.sort((a, b) => a - b)
+// console.log("numberArr", numberArr)
+// console.log("numberArr" , numberArr.toSorted())
+
+// const sortArr = numberArr.sort()
+// console.log(numberArr, "numberArr")
+
+
+// module.export = arr
+
+// const age = 20
+
+// if (age === 20) {
+//     console.log("ALLOW")
+// } else {
+//     console.log("not ALLOW")
+// }
+
+// ternary opt
+
+// ? 
+// :
+
+// const age = 20
+// const isCheck = age === 20 ? "Allow" : "Not Allow"
+// console.log(isCheck)
+
+
+
+// const age = 20
+
+// const isCheck = age === 18 ? "Allow" : age === 20 ? "Allow" : "not allow"
+// console.log("isCheck", isCheck)
+
+
---
+++ Advance JavaScript/class 05/app1.js
@@ -0,0 +1,12 @@
+// console.log(arr, "app1.js")
+// import appArr from "./app.js" //default export
+// // import { firstName, lastName } from "./app.js"
+// import * as nameKuchbhi from "./app.js" //alias import 
+
+
+// console.log(nameKuchbhi.default, "nameKuchbhi")
+// console.log(appArr, "appArr")
+// console.log(firstName, "firstName")
+
+// const arr = require("./app.js")
+// const {arr} = require("./app.js")
